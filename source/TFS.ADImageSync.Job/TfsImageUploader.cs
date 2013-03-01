0247037ef593f5ba8b28e6f7dc32ea0ff220a5aa
Daniel Sack <daniel@sack.at>
2013-03-01 23:50:24 +0100

    Refactoring of Job as preperation for Gravatar Implementation

---
+++ source/TFS.ADImageSync.Job/TfsImageUploader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.TeamFoundation.Framework.Server;
+using muhaha.Utils.Drawing.Imaging;
+
+namespace muhaha.TFS.Jobs.ImageSync
+{
+    public static class TfsImageUploader
+    {
+        public static Tuple<TeamFoundationJobExecutionResult, string> Run(TeamFoundationRequestContext requestContext, Func<TeamFoundationIdentity, byte[]> imageProviderFunc)
+        {
+            string resultMessage = "";
+            try
+            {
+                var service = requestContext.GetService<TeamFoundationIdentityService>();
+
+                var identities = service.ReadFilteredIdentities(requestContext,
+                                                                "Microsoft.TeamFoundation.Identity.DisplayName CONTAINS '' AND Microsoft.TeamFoundation.Identity.Type == 'User'",
+                                                                5000, null, true, MembershipQuery.None);
+                if (identities != null)
+                {
+                    foreach (TeamFoundationIdentity identity in identities.Items)
+                    {
+                        //KeyValue<string, string>[] attributesSet = identity.AttributesSet;
+                        //var enumerable = attributesSet.Select(i => i.Key + " - " + i.Value).ToList();
+                        //enumerable.Insert(0, identity.DisplayName);
+                        //var contents = string.Join(Environment.NewLine, enumerable);
+                        ////ConfirmedNotificationAddress
+                        //File.AppendAllText("C:\temp\attributes", contents);
+
+                        //var adImage = ADHelper.GetImageFromAD(identity.UniqueName);
+                        var adImage = imageProviderFunc(identity);
+                        if (adImage == null)
+                            continue;
+
+                        UploadImage(requestContext, identity, service, adImage);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                resultMessage = e.Message + e.StackTrace;
+                return Tuple.Create(TeamFoundationJobExecutionResult.Failed, resultMessage);
+            }
+
+            return Tuple.Create(TeamFoundationJobExecutionResult.Succeeded, resultMessage);
+        }
+
+        private static void UploadImage(TeamFoundationRequestContext requestContext, TeamFoundationIdentity identity, TeamFoundationIdentityService service, byte[] adImage)
+        {
+            //to quadrasize image
+            var tuple = QuadrasizeImage(adImage);
+            var tfsImageFormat = tuple.Item2;
+            var tfsImage = tuple.Item1;
+
+            var propertyUpdates = new List<PropertyValue>
+                {
+                    new PropertyValue("Microsoft.TeamFoundation.Identity.Image.Data", tfsImage),
+                    new PropertyValue("Microsoft.TeamFoundation.Identity.Image.Type", "image/" + tfsImageFormat.ToString()),
+                    new PropertyValue("Microsoft.TeamFoundation.Identity.Image.Id", Guid.NewGuid().ToByteArray()),
+                    new PropertyValue("Microsoft.TeamFoundation.Identity.CandidateImage.Data", null),
+                    new PropertyValue("Microsoft.TeamFoundation.Identity.CandidateImage.UploadDate", null),
+                };
+
+            service.UpdateExtendedProperties(requestContext, identity.Descriptor, propertyUpdates);
+            service.RefreshIdentity(requestContext, identity.Descriptor);
+        }
+
+        private static Tuple<byte[], ImageFormat> QuadrasizeImage(byte[] adImage)
+        {
+            Image image = ImageHelper.ByteArrayToImage(adImage);
+            Image newImage = ImageHelper.QuadrasizeImage(image);
+            byte[] tfsImage = ImageHelper.ImageToByteArray(newImage, System.Drawing.Imaging.ImageFormat.Png);
+            ImageFormat tfsImageFormat = ImageFormatHelper.GetImageFormat(tfsImage);
+            return Tuple.Create(tfsImage, tfsImageFormat);
+        }
+    }
+}
--- source/TFS.GravatarSync.Job/SyncImagesJob.cs
+++ source/TFS.GravatarSync.Job/SyncImagesJob.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using Microsoft.TeamFoundation.Framework.Server;
 using muhaha.TFS.Jobs.ImageSync;
-using muhaha.Utils.DirectoryServices;
 
-namespace muhaha.TFS.ADImageSync.Job
+namespace muhaha.TFS.GravatarSync.Job
 {
     public class SyncImagesJob : ITeamFoundationJobExtension
     {
-        public static readonly Guid JobId = new Guid("66590D0D-3D89-4A04-878A-2204E9077E50");
-        public const string JobName = "AD Image Sync Job";
+        public static readonly Guid JobId = new Guid("8DB6C6A0-8C3E-476F-B7E0-21E692C1A53C");
+        public const string JobName = "Gravatar Image Sync Job";
 
         public TeamFoundationJobExecutionResult Run(TeamFoundationRequestContext requestContext, TeamFoundationJobDefinition jobDefinition, DateTime queueTime, out string resultMessage)
         {
-            Func<TeamFoundationIdentity, byte[]> imageProviderFunc = i => ADHelper.GetImageFromAD(i.UniqueName); //todo => getimage from AD Func
+            //https://gist.github.com/danesparza/973923
+            //http://danesparza.net/2010/10/using-gravatar-images-with-c-asp-net/
+            //http://www.codeproject.com/Articles/332404/Gravatar-avatars-in-Csharp-for-NET
+            Func<TeamFoundationIdentity, byte[]> imageProviderFunc = null; //todo => getimage from Gravatar
             var run = TfsImageUploader.Run(requestContext, imageProviderFunc);
             resultMessage = run.Item2;
             return run.Item1;
